bca474a387a05dbf0e0561246bbf21761b8b0eb5
Mattias Eriksson <thematerik@gmail.com>
2015-03-07 12:19:12 -0800

    MOVED the index file from the lib folder

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-module.exports = require('./lib/');
+var _ = require('underscore');
+var bodyParser = require('body-parser');
+var errors = require('restberry-errors');
+var express = require('express');
+var modules = require('restberry-modules');
+
+
+function RestberryExpress() {
+    this.express = express;
+    this.app = null;
+};
+
+RestberryExpress.prototype.__proto__ = modules.waf.prototype;
+
+RestberryExpress.prototype.del = function() {
+    var app = this.app;
+    app.del.apply(app, arguments);
+};
+
+RestberryExpress.prototype.get = function() {
+    var app = this.app;
+    app.get.apply(app, arguments);
+};
+
+RestberryExpress.prototype.listen = function(port, next) {
+    this.app.listen(port, next);
+};
+
+RestberryExpress.prototype.post = function() {
+    var app = this.app;
+    app.post.apply(app, arguments);
+};
+
+RestberryExpress.prototype.put = function() {
+    var app = this.app;
+    app.put.apply(app, arguments);
+};
+
+RestberryExpress.prototype.res = function(code, data) {
+    this._res.status(code).json(data);
+};
+
+RestberryExpress.prototype.use = function(next) {
+    var self = this;
+    var app = express();
+    app.use(bodyParser.json());
+    self.app = app;
+    if (next)  next(self);
+    return self;
+};
+
+module.exports = exports = new RestberryExpress;
